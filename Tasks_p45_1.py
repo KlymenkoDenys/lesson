04ab532cb717acb4bebd696265337ced83385424
klymenkDenys <klimenkdv@yahoo.com>
2020-12-09 13:37:05 +0200

    p42_2

---
+++ Tasks_p45_1.py
@@ -0,0 +1,12 @@
+# Game over - ver.2
+# This program is showing techniques for working with strings
+print("Program 'Game Over' ver. 2.0")
+print("Also". "most". "message")
+print("The same".
+      "the same".
+      message")
+print("This". end="")
+print("it...")
+print("""
+"""
+""")
