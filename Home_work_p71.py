b75489697d870ac6c9535889d08bddb998a19d06
klymenkDenys <klimenkdv@yahoo.com>
2021-02-09 13:16:56 +0200

    1

---
+++ Home_work_p71.py
@@ -0,0 +1,15 @@
+# Соединяет два ваших блюда
+
+first = input("Первое блюдо: ")
+second = input("Второе блюдо: ")
+print(first + second)
+input("\nНажмите Enter, чтобы выйти.")
+
+# Расчет чаевых 15% и 20%
+
+insert = int(input("Расчет чаевых 20%: "))
+print(insert * 0.2)
+print(insert * 0.15)
+
+input("\nНажмите Enter, чтобы выйти")
+
